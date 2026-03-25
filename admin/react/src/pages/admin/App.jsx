c9bb5a6843b30cb144bbba9c7665944cf3d7f314
liaohongdong <liaodoho@gmail.com>
2026-03-25 19:21:04 +0800

    wip

--- admin/react/src/pages/admin/App.jsx
+++ admin/react/src/pages/admin/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import './style.scss'
 import React from 'react'
-import { Button, Space, DatePicker, version, Tabs } from 'antd'
+import { App, Button, Space, DatePicker, version, Tabs } from 'antd'
 import Children from './components/Children'
 
 const onChange = key => {
   console.log(key, 8);
 };
 
-const App = () => {
+const _App = () => {
   console.log(window, 998123, window.__params, window.$);
   const items = [];
   if (window.__params?.admin_options?.tab_options && window.__params.admin_options.tab_options?.length) {
@@ -24,11 +24,11 @@ const App = () => {
     
   }
   return (
-    <div className="wrapper">
+    <App className="wrapper">
       {/* <h1 className="tw:text-[50px]! tw:font-bold tw:underline">antd version: {version}</h1> */}
       <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
-    </div>
+    </App>
   )
 }
 
-export default App
+export default _App
--- admin/react/src/pages/admin/components/GlobalSet.jsx
+++ admin/react/src/pages/admin/components/GlobalSet.jsx
@@ -1,11 +1,47 @@
-import { Input } from 'antd'
+import { Input, List, Radio, Divider, Typography } from 'antd'
 
 const GlobalSet = (props) => {
   const { item } = props;
   const { global_set } = item;
   const { item: _item } = global_set;
-  console.log(item, 555, global_set, _item);
-  
-  return <>GlobalSet {JSON.stringify(item)}</>
+  const dom = <>
+    <List
+      bordered
+      dataSource={_item}
+      renderItem={(item) => (
+        <List.Item
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'start',
+          }}>
+          <div className='tw:text-lg tw:font-bold'>
+            <Typography.Text level={5}>{item.title}</Typography.Text>
+          </div>
+          {['radio'].includes(item.type) && (
+            <Radio.Group
+              value={item.default}
+              options={
+                item.options.map(option => ({
+                  value: option.key,
+                  label: option.value
+                }))
+              }
+            />
+          )}
+          <div>
+            <Typography.Text level={5}
+              style={{
+                fontSize: '12px',
+                color: '#6b7280',
+              }}>{item.desc}</Typography.Text>
+          </div>
+        </List.Item>
+      )}
+    />
+  </>
+
+  // return <>GlobalSet {JSON.stringify(item)}</>
+  return dom
 }
 export default GlobalSet
