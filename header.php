978e43312762e5eb4575be192a95ef783371c144
liaohongdong <liaodoho@gmail.com>
2026-07-20 14:12:50 +0800

    wip

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> data-theme="<?php echo hash_config('theme_mode') ?: 'light'; ?>">
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
